67b9c0bedda8a4923bd3a612426fdd7928fa0376
mazj <mazhangjing@inspur.com>
2022-05-31 19:46:42 +0800

    add chapter 33 learn

--- chapter33/select.c
+++ chapter33/select.c
@@ -3,9 +3,19 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <winsock.h>
 
 int main(int argc, char** argv) {
     while (1) {
-        
+        fd_set readFDs;
+        FD_ZERO(&readFDs);
+        int fd;
+        for (fd = minFD; fd < maxFD; fd++)
+            FD_SET(fd, &readFDs);
+        int rc = select(maxFD+1,&readFDs, NULL,NULL,NULL);
+        int fd;
+        for (fd = minFD; fd < maxFD; fd++)
+            if (FD_ISSET(fd, &readFDs))
+                processFD(fd);
     }
 }
