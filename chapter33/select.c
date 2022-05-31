ce23a4d0a4c7d75b5d6d4fa62917837f08cefbc2
corkine <mzj960032>
2022-05-31 19:42:57 +0800

    upload chapter32 learn

---
+++ chapter33/select.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+    while (1) {
+        
+    }
+}
